f90f92ba286eb192b7f3a8df12368301177ffe8f
Marcus Olsson <marcus.olsson@ericsson.com>
2020-02-05 16:53:58 +0100

    Run evictFromMembership in GossipStage
    patch by Marcus Olsson; reviewed by X for CASSANDRA-15592

--- src/java/org/apache/cassandra/gms/Gossiper.java
+++ src/java/org/apache/cassandra/gms/Gossiper.java
@@ -862,7 +862,7 @@ public class Gossiper implements IFailureDetectionEventListener, GossiperMBean
                     {
                         logger.debug("time is expiring for endpoint : {} ({})", endpoint, expireTime);
                     }
-                    evictFromMembership(endpoint);
+                    runInGossipStageBlocking(() -> evictFromMembership(endpoint));
                 }
             }
         }
